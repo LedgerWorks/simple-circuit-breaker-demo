e70892a29c46a16da4645007ba07d416542e5b68
Erik Muir <erikdmuir@gmail.com>
2023-05-25 15:26:53 -0500

    remove bad map function

--- src/lib/contract-calls/get-current-flip.ts
+++ src/lib/contract-calls/get-current-flip.ts
@@ -1,9 +1,8 @@
 import { getPlayerAddress } from "../../../common/env";
 import { getContractForPlayer } from "../../../common/ethers-helpers";
 import { Flip } from "../../../common/types";
-import { map } from "../mappers";
 
 export const getCurrentFlip = async (): Promise<Flip> => {
   const rawFlip = await getContractForPlayer().getCurrentFlip(getPlayerAddress());
-  return map<Flip>(rawFlip);
+  return Object.fromEntries(Object.entries(rawFlip)) as Flip;
 };
